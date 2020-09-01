b689ad19a92bd8de9dbdd9781f5209559de51e89
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2020-09-01 17:11:23 +0900

    step1

--- src/usecase/feseventinteractor/fesevent_interactor_test.go
+++ src/usecase/feseventinteractor/fesevent_interactor_test.go
@@ -1,6 +1,7 @@
 package feseventinteractor_test
 
 import (
+	"cleanarchitecture-fes/src/adaptor/repository"
 	"cleanarchitecture-fes/src/domain"
 	"cleanarchitecture-fes/src/usecase/feseventinteractor"
 	"log"
@@ -10,21 +11,35 @@ import (
 	"github.com/tj/assert"
 )
 
-func TestFesEventUsecase(t *testing.T) {
-	os.Setenv("MYSQL_ARGS", "cafes:cafes99@(localhost:3306)/cleanarchitecture_fes")
-	t.Run("SaveFesEvent", func(t *testing.T) {
-		inputFesEvent := domain.FesEvent{
-			Title:   "FesEventTestDummy",
-			Speaker: "FesEventSpeakerDummy",
-		}
-		uc := feseventinteractor.New()
-		err := uc.Save(inputFesEvent)
-		assert.Nil(t, err)
-	})
-	t.Run("GetFesEvent", func(t *testing.T) {
-		uc := feseventinteractor.New()
-		events, err := uc.Get()
-		assert.Nil(t, err)
-		log.Print(events)
-	})
+func TestFesEventSaveUsecase(t *testing.T) {
+	args := os.Getenv("MYSQL_ARGS")
+	if args == "" {
+		panic("Please set MYSQL_ARGS env")
+	}
+	fesEventRepository, err := repository.New(args)
+	if err != nil {
+		panic("repository init error")
+	}
+	uc := feseventinteractor.New(fesEventRepository)
+	inputFesEvent := domain.FesEvent{
+		Title:   "FesEventTestDummy",
+		Speaker: "FesEventSpeakerDummy",
+	}
+	err = uc.Save(inputFesEvent)
+	assert.Nil(t, err)
+}
+
+func TestFesEventGetUsecase(t *testing.T) {
+	args := os.Getenv("MYSQL_ARGS")
+	if args == "" {
+		panic("Please set MYSQL_ARGS env")
+	}
+	fesEventRepository, err := repository.New(args)
+	if err != nil {
+		panic("repository init error")
+	}
+	uc := feseventinteractor.New(fesEventRepository)
+	fesEvents, err := uc.Get()
+	assert.Nil(t, err)
+	log.Printf("GetFesEvents: %v", fesEvents)
 }
